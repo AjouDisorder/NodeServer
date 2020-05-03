4aa116f7c9a90f19544962d59bb64270202ae893
MTG <rjwltps57@ajou.ac.kr>
2020-05-03 22:23:58 +0900

    init functions in boss services dir

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var userWishListService = require('./lib/userServices/userWishListService');
 var userFavoriteService = require('./lib/userServices/userFavoriteService');
 var userReviewService = require('./lib/userServices/userReviewService');
 var bossRestaurantService = require('./lib/bossServices/bossRestaurantManageService');
-var bossOriginService = require('./lib/bossServices/bossOriginMenuService');
+var bossOriginMenuService = require('./lib/bossServices/bossOriginMenuService');
 var bossMenuService = require('./lib/bossServices/bossMenuService');
 var bossTicketService = require('./lib/bossServices/bossTicketService');
 var bossReviewService = require('./lib/bossServices/bossReviewService');
@@ -107,53 +107,53 @@ var app = http.createServer(function(request,response){
     
     //bossServices - restaurant manage service
     else if (pathname === '/boss/getRestaurantList'){
-      
+      bossRestaurantService.getRestaurantList(request, response)
     }else if (pathname === '/boss/createRestaurant'){
-      
+      bossRestaurantService.createRestaurant(request, response)
     }else if (pathname === '/boss/updateRestaurant'){
-      
+      bossRestaurantService.updateRestaurant(request, response)
     }else if (pathname === '/boss/deleteRestaurant'){
-      
+      bossRestaurantService.deleteRestaurant(request, response)
     }
     //bossServices - origin menu service
     else if (pathname === '/boss/createOriginMenu'){
-      
+      bossOriginMenuService.createOriginMenu(request, response)
     }else if (pathname === '/boss/updateOriginMenu'){
-      
+      bossOriginMenuService.updateOriginMenu(request, response)
     }else if (pathname === '/boss/deleteOriginMenu'){
-      
+      bossOriginMenuService.deleteOriginMenu(request, response)
     }else if (pathname === '/boss/getOriginMenuList'){
-      
+      bossOriginMenuService.getOriginMenuList(request, response)
     }
     //bossServices - menu service
     else if (pathname === '/boss/createMenu'){
-      
+      bossMenuService.createMenu(request, response)
     }else if (pathname === '/boss/updateMenu'){
-      
+      bossMenuService.updateMenu(request, response)
     }else if (pathname === '/boss/deleteMenu'){
-      
+      bossMenuService.deleteMenu(request, response)
     }
     //bossServices - ticket service
     else if (pathname === '/boss/getPaidTicketList'){
-      
+      bossTicketService.getPaidTicketList(request, response)
     }else if (pathname === '/boss/setTicketDisable'){
-      
+      bossTicketService.setTicketDisable(request, response)
     }else if (pathname === '/boss/getCertifiedTicketList'){
-      
+      bossTicketService.getCertifiedTicketList(request, response)
     }
     //bossServices - review service
     else if (pathname === '/boss/getReviewList'){
-      
+      bossReviewService.getReviewList(request, response)
     }else if (pathname === '/boss/createReply'){
-      
+      bossReviewService.createReply(request, response)
     }else if (pathname === '/boss/updateReply'){
-      
+      bossReviewService.updateReply(request, response)
     }else if (pathname === '/boss/deleteReply'){
-      
+      bossReviewService.deleteReply(request, response)
     }
     //bossServices - info service
     else if (pathname === '/boss/updateInfo'){
-      
+      bossInfoService.updateInfo(request, response)
     }
     else {
       response.writeHead(404);
