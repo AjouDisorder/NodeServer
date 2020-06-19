1f214d8754d53792b483e788769018aead224e77
dnwls514@ajou.ac.kr <dnwls514@ajou.ac.kr>
2020-06-19 21:21:12 +0900

    token refresh updated

--- NodeServer/lib/loginService.js
+++ NodeServer/lib/loginService.js
@@ -1,6 +1,8 @@
 var db = require('./mongoCollections')
 var User_DB = db.collection_user()
 var Boss_DB = db.collection_boss()
+var Restaurant_DB = db.collection_restaurant()
+var Menu_DB = db.collection_menu()
 
 module.exports = {
     userSignup: function(req, res){
@@ -87,12 +89,22 @@ module.exports = {
                     result.save((err2, updatedBoss)=>{
                         if(updatedBoss){
                             res.json(updatedBoss)
-                            console.log(updatedBoss)
+                            Restaurant_DB.find({'_id': { $in: updatedBoss.restaurantidList } }, (err3, restaurantList)=>{
+                                for (var i = 0; i<restaurantList.length; i++){
+                                    restaurantList[i].token = token
+                                    Menu_DB.find({'_id': { $in: restaurantList[i].menuidList} }, (err4, targetMenuList)=>{
+                                        for (var i = 0; i<targetMenuList.length; i++){
+                                            targetMenuList[i].token = token
+                                            targetMenuList[i].save(()=>{ })
+                                        }
+                                    })
+                                }
+                            })
                         }
                         else{
                             res.send({"result" : "token refresh failed"});
-
                         }
+                        
                     })
                 }
                 else{
